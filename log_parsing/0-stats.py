8ed30e5e787657ad08ebae19136ff7ea351c321f
Eidal559 <eidaljuarbe@gmail.com>
2024-10-08 21:46:14 -0400

    fix

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -15,8 +15,9 @@ if __name__ == "__main__":
             code = int(words[-2])
             if code in codes:
                 codes[code] += 1
-        except:
+        except ValueError:
             pass
+
 
     def print_stats():
         '''Prints accumulated statistics.'''
