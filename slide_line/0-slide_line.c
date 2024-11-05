c880ca70905d4ce98f92dbb8edeada8a15903df0
Eidal559 <eidaljuarbe@gmail.com>
2024-11-04 22:10:23 -0400

    updated file

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,126 +1,108 @@
 #include "slide_line.h"
 
-/* Function prototypes */
-void slide_left(int *line, size_t size);
-void slide_right(int *line, size_t size);
-
 /**
- * slide_line - Slides and merges an array of integers
- * @line: Points to an array of integers containing `size` elements
- * @size: Number of elements in `line`
- * @direction: Direction to slide and merge (SLIDE_LEFT or SLIDE_RIGHT)
+ * slide_line - slides an array to left or right to sum numbers
  *
- * Return: 1 upon success, 0 upon failure
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * @direction: left or right
+ * Return: 1 if is possible or 0 if it's not possible
  */
 
 int slide_line(int *line, size_t size, int direction)
 {
-    if (line == NULL || size == 0 || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
-        return 0;
-
-    if (direction == SLIDE_LEFT)
-        slide_left(line, size);
-    else
-        slide_right(line, size);
-
-    return 1;
-}
-
-
-/**
- * slide_left - Helper function to slide and merge elements to the left
- * @line: Points to an array of integers containing `size` elements
- * @size: Number of elements in `line`
- */
-
-void slide_left(int *line, size_t size)
-{
-    size_t i, pos = 0;
-
-    /* First pass: Slide all non-zero values to the left */
-    for (i = 0; i < size; i++)
-    {
-        if (line[i] != 0)
-        {
-            line[pos++] = line[i];
-        }
-    }
-    /* Fill the rest with zeros */
-    for (i = pos; i < size; i++)
-    {
-        line[i] = 0;
-    }
-
-    /* Second pass: Merge adjacent values and slide again */
-    for (i = 0; i < size - 1; i++)
-    {
-        if (line[i] == line[i + 1] && line[i] != 0)
-        {
-            line[i] *= 2;
-            line[i + 1] = 0;
-        }
-    }
-
-    /* Final pass: Slide all non-zero values to the left again */
-    pos = 0;
-    for (i = 0; i < size; i++)
-    {
-        if (line[i] != 0)
-        {
-            line[pos++] = line[i];
-        }
-    }
-    for (i = pos; i < size; i++)
-    {
-        line[i] = 0;
-    }
+	if (direction == 2)
+		return (to_left(line, size));
+	if (direction == 1)
+		return (to_right(line, size));
+	else
+		return (0);
 }
 
 /**
- * slide_right - Helper function to slide and merge elements to the right
- * @line: Points to an array of integers containing `size` elements
- * @size: Number of elements in `line`
+ * to_left - slide to the left to sum
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * Return: 1 on success
  */
 
-void slide_right(int *line, size_t size)
+int to_left(int *line, size_t size)
 {
-    size_t i, pos = size - 1;
+	int cur = 0, nxt = 0;
+	size_t i, index = 0;
 
-    /* First pass: Slide all non-zero values to the right */
-    for (i = size; i-- > 0;)
-    {
-        if (line[i] != 0)
-        {
-            line[pos--] = line[i];
-        }
-    }
-    /* Fill the rest with zeros */
-    for (i = 0; i <= pos; i++)
-    {
-        line[i] = 0;
-    }
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			nxt = line[i];
+		if (cur != 0 && nxt != 0)
+		{
+			if (cur == nxt)
+			{
+				line[index++] = cur + nxt;
+				cur = 0;
+				nxt = 0;
+			}
+			else
+			{
+				line[index++] = cur;
+				cur = nxt;
+				nxt = 0;
+				if (i == size - 1)
+					line[index++] = cur;
+			}
+		}
+		if (cur != nxt && i == size - 1)
+			line[index++] = cur;
+	}
+	for (i = index; i < size; i++)
+		line[i] = 0;
+	return (1);
+}
 
-    /* Second pass: Merge adjacent values and slide again */
-    for (i = size - 1; i > 0; i--)
-    {
-        if (line[i] == line[i - 1] && line[i] != 0)
-        {
-            line[i] *= 2;
-            line[i - 1] = 0;
-        }
-    }
+/**
+ * to_right - slides an array to sum numbers
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * Return: 1 on success
+ */
 
-    /* Final pass: Slide all non-zero values to the right again */
-    pos = size - 1;
-    for (i = size; i-- > 0;)
-    {
-        if (line[i] != 0)
-        {
-            line[pos--] = line[i];
-        }
-    }
-    for (i = 0; i <= pos; i++)
-    {
-        line[i] = 0;
-    }
+int to_right(int *line, size_t size)
+{
+	int cur = 0, previous = 0;
+	size_t i, index = size - 1;
+
+	for (i = size - 1; i < size; i--)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			previous = line[i];
+		if (cur != 0 && previous != 0)
+		{
+			if (cur == previous)
+			{
+				line[index--] = cur + previous;
+				cur = 0;
+				previous = 0;
+			}
+			else
+			{
+				line[index--] = cur;
+				cur = previous;
+				previous = 0;
+				if (i == 0)
+					line[index--] = cur;
+			}
+		}
+		else if (cur != previous && i == 0)
+			line[index--] = cur;
+	}
+	for (i = 0; i < index + 1; i++)
+		line[i] = 0;
+	return (1);
 }
