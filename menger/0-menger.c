fb8e84a178eee08b3aa6de67eafd6458e9a41491
Eidal559 <eidaljuarbe@gmail.com>
2024-11-11 22:31:48 -0400

    fix

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -10,32 +10,32 @@
  */
 void menger(int level)
 {
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    int size = pow(3, level);
+	int size = pow(3, level);
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            int x = i, y = j;
-            char symbol = '#';
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			int x = i, y = j;
+			char symbol = '#';
 
-            /* Determine if the current position should be empty */
-            while (x > 0 || y > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    symbol = ' ';
-                    break;
-                }
-                x /= 3;
-                y /= 3;
-            }
+			/* Determine if the current position should be empty */
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					symbol = ' ';
+					break;
+				}
+				x /= 3;
+				y /= 3;
+			}
 
-            putchar(symbol);
-        }
-        putchar('\n');
-    }
+			putchar(symbol);
+		}
+		putchar('\n');
+	}
 }
