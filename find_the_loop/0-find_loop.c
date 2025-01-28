b48e69261b76f43a2bf5fc98f2ba459a4ff5fe66
Eidal559 <eidaljuarbe@gmail.com>
2025-01-28 16:52:00 -0400

    fix

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -8,29 +8,29 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow, *fast;
+	listint_t *slow, *fast;
 
-    if (head == NULL || head->next == NULL)
-        return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-    slow = head;
-    fast = head;
+	slow = head;
+	fast = head;
 
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow);
-        }
-    }
-    return (NULL);
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
 }
