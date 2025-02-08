85a27efa01d4ce389137de8e2574d94c9f90dbc9
Eidal559 <eidaljuarbe@gmail.com>
2025-02-08 13:15:19 -0400

    task-0

---
+++ merge_sort/merge_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * merge - Merges two sorted subarrays into one sorted array.
+ * @array: The original array being sorted.
+ * @temp: A temporary array used for merging.
+ * @left: Left boundary index.
+ * @mid: Middle index.
+ * @right: Right boundary index.
+ */
+void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
+{
+    size_t i = left, j = mid, k = left;
+
+    printf("Merging...\n");
+    while (i < mid && j < right)
+    {
+        if (array[i] <= array[j])
+            temp[k++] = array[i++];
+        else
+            temp[k++] = array[j++];
+    }
+    while (i < mid)
+        temp[k++] = array[i++];
+    while (j < right)
+        temp[k++] = array[j++];
+
+    for (i = left; i < right; i++)
+        array[i] = temp[i];
+
+    printf("[Done]: ");
+    print_array(array + left, right - left);
+}
+
+/**
+ * merge_sort_recursive - Recursively splits and sorts an array.
+ * @array: The array to be sorted.
+ * @temp: A temporary array for merging.
+ * @left: Left boundary index.
+ * @right: Right boundary index.
+ */
+void merge_sort_recursive(int *array, int *temp, size_t left, size_t right)
+{
+    if (right - left < 2)
+        return;
+
+    size_t mid = left + (right - left) / 2;
+    merge_sort_recursive(array, temp, left, mid);
+    merge_sort_recursive(array, temp, mid, right);
+    merge(array, temp, left, mid, right);
+}
+
+/**
+ * merge_sort - Sorts an array of integers using Merge Sort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void merge_sort(int *array, size_t size)
+{
+    if (!array || size < 2)
+        return;
+
+    int *temp = malloc(size * sizeof(int));
+    if (!temp)
+        return;
+
+    merge_sort_recursive(array, temp, 0, size);
+    free(temp);
+}
