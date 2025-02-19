d34a403510685fef109f4490f6d90f5cc0f7dfc7
Eidal559 <eidaljuarbe@gmail.com>
2025-02-18 20:15:21 -0400

    task-0

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,66 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * add_node_end - Adds a new node to the end of a circular doubly linked list
+ * @list: Pointer to the head of the list
+ * @str: String to store in the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+    List *new_node, *tail;
+
+    if (!list || !str)
+        return (NULL);
+
+    new_node = malloc(sizeof(List));
+    if (!new_node)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (!new_node->str)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    if (*list == NULL)
+    {
+        /* First node in an empty list */
+        new_node->next = new_node;
+        new_node->prev = new_node;
+        *list = new_node;
+    }
+    else
+    {
+        tail = (*list)->prev;
+        new_node->next = *list;
+        new_node->prev = tail;
+        tail->next = new_node;
+        (*list)->prev = new_node;
+    }
+
+    return (new_node);
+}
+
+/**
+ * add_node_begin - Adds a new node to the beginning of a circular doubly linked list
+ * @list: Pointer to the head of the list
+ * @str: String to store in the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+    List *new_node = add_node_end(list, str);
+
+    if (!new_node)
+        return (NULL);
+
+    /* Set the new node as the head of the list */
+    *list = new_node;
+
+    return (new_node);
+}
