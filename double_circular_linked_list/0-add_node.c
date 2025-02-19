31842b09d278de3d138669f15b516e1d9529fde1
Eidal559 <eidaljuarbe@gmail.com>
2025-02-18 20:23:30 -0400

    fix

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -45,7 +45,8 @@ List *add_node_end(List **list, char *str)
 }
 
 /**
- * add_node_begin - Adds a new node to the beginning of a circular doubly linked list
+ * add_node_begin - Adds a new node to the beginning of 
+ * a circular doubly linked list
  * @list: Pointer to the head of the list
  * @str: String to store in the new node
  *
