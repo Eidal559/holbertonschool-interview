9d4452a06323426edff98c77d9d7aaae4ab7f4f3
Eidal559 <eidaljuarbe@gmail.com>
2025-04-12 16:47:55 -0400

    fixes

--- substring/substring.c
+++ substring/substring.c
@@ -3,6 +3,55 @@
 #include <string.h>
 
 /**
+ * is_valid_combination - Checks if current window contains all required words
+ * @s: The source string
+ * @i: Starting index in source string
+ * @words: Array of words to find
+ * @nb_words: Number of words in the array
+ * @word_len: Length of each word
+ *
+ * Return: 1 if valid combination found, 0 otherwise
+ */
+static int is_valid_combination(char const *s, int i, char const **words,
+		int nb_words, int word_len)
+{
+	int *found = calloc(nb_words, sizeof(int));
+
+	if (!found)
+		return (0);
+
+	int words_found = 0;
+	int valid = 1;
+
+	for (int j = 0; j < nb_words && valid; j++)
+	{
+		char temp[word_len + 1];
+
+		strncpy(temp, s + i + j * word_len, word_len);
+		temp[word_len] = '\0';
+
+		int word_found = 0;
+
+		for (int k = 0; k < nb_words; k++)
+		{
+			if (!found[k] && strcmp(temp, words[k]) == 0)
+			{
+				found[k] = 1;
+				words_found++;
+				word_found = 1;
+				break;
+			}
+		}
+
+		if (!word_found)
+			valid = 0;
+	}
+
+	free(found);
+	return (valid && words_found == nb_words);
+}
+
+/**
  * find_substring - Finds all possible substrings containing a list of words
  * @s: String to scan
  * @words: Array of words all substrings must be a concatenation of
@@ -21,75 +70,27 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 	int word_len = strlen(words[0]);
 	int total_substr_len = word_len * nb_words;
 
-	/* If string shorter than all words concatenated, return NULL */
 	if (s_len < total_substr_len)
 		return (NULL);
 
-	/* Allocate memory for indices */
 	int max_matches = s_len - total_substr_len + 1;
 	int *indices = malloc(max_matches * sizeof(int));
 
 	if (!indices)
 		return (NULL);
 
-	/* For each possible starting position in the main string */
 	for (int i = 0; i <= s_len - total_substr_len; i++)
 	{
-		/* Create array to mark which words we've found */
-		int *found = calloc(nb_words, sizeof(int));
-
-		if (!found)
-		{
-			free(indices);
-			return (NULL);
-		}
-
-		int words_found = 0;
-		int valid = 1;
-
-		/* Check each possible word position in the current substring window */
-		for (int j = 0; j < nb_words && valid; j++)
-		{
-			/* Extract the current word-sized chunk from the string */
-			char temp[word_len + 1];
-
-			strncpy(temp, s + i + j * word_len, word_len);
-			temp[word_len] = '\0';
-
-			int word_found = 0;
-
-			/* Try to match this chunk with each word */
-			for (int k = 0; k < nb_words; k++)
-			{
-				if (!found[k] && strcmp(temp, words[k]) == 0)
-				{
-					found[k] = 1;
-					words_found++;
-					word_found = 1;
-					break;
-				}
-			}
-
-			/* If chunk doesn't match any word, this substring is invalid */
-			if (!word_found)
-				valid = 0;
-		}
-
-		free(found);
-
-		/* If all words found in this substring, add index to result */
-		if (valid && words_found == nb_words)
+		if (is_valid_combination(s, i, words, nb_words, word_len))
 			indices[(*n)++] = i;
 	}
 
-	/* If no indices found, free the array and return NULL */
 	if (*n == 0)
 	{
 		free(indices);
 		return (NULL);
 	}
 
-	/* Resize the array to the actual number of indices found */
 	int *result = realloc(indices, (*n) * sizeof(int));
 
 	return (result ? result : indices);
